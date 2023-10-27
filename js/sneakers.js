503d446b9e42e7c3fb2d315c9b2dfd7d03bdc320
tinatorres122 <valentinagto12@gmail.com>
2023-10-27 11:52:10 -0500

    cambio por

--- js/sneakers.js
+++ js/sneakers.js
@@ -11,6 +11,13 @@ let thumb2 = document.getElementById("thumb2");
 let thumb3 = document.getElementById("thumb3");
 let thumb4 = document.getElementById("thumb4");
 let botonCerrar = document.getElementById("botonCerrar");
+let boxMinus = document.getElementById("boxMinus");
+let boxCantidad = document.getElementById("boxCantidad");
+let boxPlus = document.getElementById("boxPlus");
+let valorUnitario =180000;
+
+
+
 
 mainImg.addEventListener("click", function() {
     boxImgFixed.classList.remove("box-img-fixed-hidden");
@@ -76,4 +83,27 @@ thumb4.addEventListener("click", function() {
 botonCerrar.addEventListener("click", function() {
     boxImgFixed.classList.add("box-img-fixed-hidden");
     boxImgFixed.classList.remove("box-img-fixed");
+});
+let valorUnitarioAux = Intl.NumberFormat("DE-de").format(valorUnitario)
+document.getElementById("precioUnitario").innerHTML = "$" + valorUnitarioAux;
+
+boxPlus.addEventListener("click", function() {
+    let cantidad = boxCantidad.innerHTML;
+    if (cantidad < 10) {
+    cantidad++; 
+    document.getElementById("boxCantidad").innerHTML = cantidad;
+    let valorTotalInt= cantidad * valorUnitario;
+    let valorTotalAux = Intl.NumberFormat("DE-de").format(valorUnitario);
+    document.getElementById("valorTotal").innerHTML = "$" + valorTotalAux;
+}
+});
+boxMinus.addEventListener("click", function() {
+    let cantidad = boxCantidad.innerHTML;
+    if (cantidad > 0) {
+    cantidad--; 
+    document.getElementById("boxCantidad").innerHTML = cantidad;
+    let valorTotalInt= cantidad * valorUnitario;
+    let valorTotalAux = Intl.NumberFormat("DE-de").format(valorUnitario);
+    document.getElementById("valorTotal").innerHTML = "$" + valorTotalAux;
+}
 });
